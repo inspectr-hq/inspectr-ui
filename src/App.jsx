1b4464db17a9babc48ef0403e844f64e2b5a26e4
Tim Haselaars <timhaselaars@gmail.com>
2025-04-29 10:57:16 +0200

    chore: Added app structure for local development

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Workspace from './components/Workspace.jsx';
+
+function App() {
+
+  return (
+    <>
+      <Workspace></Workspace>
+    </>
+  )
+}
+
+export default App
