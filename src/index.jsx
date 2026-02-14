4cf8e1f0ff4941be2de91fe98882f4e1d4d4d6f7
Thim <timhaselaars@gmail.com>
2026-02-15 00:12:23 +0100

    feat: Local Monaco Editor (#61)

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 // src/index.jsx
 
 import './styles/global.css';
+import './utils/configureMonaco.js';
 
 export * from './components/index.jsx';
 export { default as useHashRouter, parseHash } from './hooks/useHashRouter.jsx';
